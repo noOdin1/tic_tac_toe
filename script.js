284c10d141a623e9fae5621e137d2904e4ba3810
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-09-13 05:56:24 +0800

    Create player object to handle player data

--- script.js
+++ script.js
@@ -24,5 +24,19 @@ function tictactoe() {
   return { place, printBoard };
 }
 
+function player() {
+  let score = 0;
+
+  function incrementScore() {
+    score += 1;
+  }
+
+  function getScore() {
+    return score;
+  }
+
+  return { incrementScore, getScore };
+}
+
 newGame.place("x", 1, 1);
 newGame.printBoard();
